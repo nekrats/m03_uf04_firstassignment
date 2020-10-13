8c2575cd035ebf23213145f44973402be3cc6832
Pep Mendez <jmendez1@xtec.cat>
2020-10-13 20:07:49 +0200

    Añadida clase Triangullo

---
+++ src/cat/copernic/review/Triangulo.java
@@ -0,0 +1,34 @@
+/*
+ * Acaba la clase Triangulo con el/los constructores, accesores (get/set)
+ * método toString().
+ * fecha limite: 15/10/2010 19:00
+ */
+package cat.copernic.review;
+
+/**
+ *
+ * @author pep
+ */
+public class Triangulo {
+    
+    // propiedades
+    private double base;
+    private double altura;
+    
+    /* Calcula el área de este triangulo */
+    public double area() {
+        return 0.0;
+    }
+    
+    /* Calcula el perímetro de este triangulo conocida su base y altura */
+    public double perimetro() {
+        // Math.sqrt(345.9);
+        // Math.pow(n, 2.0);
+        return 0.0;
+    }
+    
+//    private double calcHipotenusa() {
+//        
+//    }
+    
+}
