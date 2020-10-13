223be5bffd4204fd606bf72c93c3135654d9f4c8
ruiz.marquez.eric <ruiz.marquez.eric@alumnat.copernic.cat>
2020-10-13 21:12:59 +0200

    Triangulo terminado

--- src/cat/copernic/review/Triangulo.java
+++ src/cat/copernic/review/Triangulo.java
@@ -15,20 +15,51 @@ public class Triangulo {
     private double base;
     private double altura;
     
+    public double getbase(){
+        return base;
+    }
+    public void setbase(double base){
+        this.base = base;
+    }
+
+    public double getaltura(){
+        return altura;
+    }
+    public void setaltura(double altura){
+        this.altura = altura;
+    }
+
     /* Calcula el área de este triangulo */
-    public double area() {
-        return 0.0;
+    public double area() throws Exception {
+        double area = (base*altura)/2;
+        if(area <=0) throws new Exception("L'area no pot ser negativa");
+        return area;
     }
-    
+    private double Hipotenusa() {
+        double hipotenusa;
+        hipotenusa = Math.sqrt(((base/2)*(base/2)) + ((altura)*(altura)));
+        return hipotenusa;
+         }
+
     /* Calcula el perímetro de este triangulo conocida su base y altura */
-    public double perimetro() {
-        // Math.sqrt(345.9);
-        // Math.pow(n, 2.0);
-        return 0.0;
+    public double perimetro(hipotenusa) {
+        double perimetro = hipotenusa*2 + base;
+        if(perimetro <=0) throws new Exception("El perimetre no pot ser negatiu");
+        return perimetro;
     }
     
-//    private double calcHipotenusa() {
-//        
-//    }
+
+    public static void main(String[] args) {
+        Triangulo t1 = new Triangulo(19.0, 17.0);
+        System.out.println(t1);
+        try {
+
+            System.out.println(t1.area());
+            System.out.println(t1.perimetro());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+       
+    }
     
 }
